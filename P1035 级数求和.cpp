85fdd2b45b66d10f2ceaa6b142b9b868666c0457
AmeL07n <18356009899@163.com>
2026-07-23 15:38:05 +0800

    Add files via upload

---
+++ P1035 级数求和.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    double sum=0.0;
+    int k;
+    cin >>k;
+    int n=0;
+    while(sum<=k){
+        n++;
+        sum+=1.0/n;
+    }
+    cout<<n<<endl;
+    return 0;
+}
