51d63374bf978c1e784395e458b2d37f4fbf81ec
AmeL07n <18356009899@163.com>
2026-08-03 23:00:58 +0800

    Add files via upload

---
+++ P1980 计数问题.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n, x;
+    cin >> n >> x;
+
+    int count = 0;
+    for (int i = 1; i <= n; i++) {
+        int num = i;
+        while (num > 0) {
+            int digit = num % 10;
+            if (digit == x) {
+                count++;
+            }
+            num /= 10;
+        }
+    }
+    cout << count << endl;
+    return 0;
+}
